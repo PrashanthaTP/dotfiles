28390d3971d1afbc74230701980c8cb3e1cd644c
PrashanthaTP <tpprashantha@gmail.com>
2021-11-25 21:22:28 +0530

    Chore(nvim): add function for file reloading
    add keybinding for vim or lua file reloading

--- .config/nvim/lua/general/leader.lua
+++ .config/nvim/lua/general/leader.lua
@@ -1,5 +1,21 @@
+--https://sbulav.github.io/vim/neovim-telescope-github/
+function _G.save_and_exec()
+	vim.cmd([[silent! write]])
+	if vim.bo.filetype == "lua" then
+		require("plenary.reload").reload_module(vim.fn.expand("%"))
+		vim.cmd([[:luafile %]])
+	elseif vim.bo.filetype == "vim" then
+        vim.cmd([[:source %]])
+	else
+	end
+
+	print("Reloaded buffer : ",vim.fn.expand("%"))
+end
+
 vim.cmd([[
+
 let mapleader="\<Space>"
+
 " Mappings with leader key
 " source vim file
 nmap <leader>vs :source $MYVIMRC<CR>
@@ -8,7 +24,7 @@ nmap <leader>vo :e $MYVIMRC<CR>
 nnoremap <leader>n :bnext<CR>
 nnoremap <leader>p :bprev<CR>
 nnoremap <leader>b :buffers<CR>
-
+nnoremap <leader>r :call v:lua.save_and_exec()<CR>
 " splits ----- {{{
 "nnoremap <leader>w <C-W>w "removed to support vimwiki
 nnoremap <leader>h <C-W>h
@@ -22,5 +38,5 @@ nnoremap <silent> <Leader>- :exe "resize " . (winheight(0) * 2/3)<CR>
 nnoremap <silent> <Leader>0 :exe "vertical resize " . (winwidth(0) * 3/2)<CR>
 nnoremap <silent> <Leader>9 :exe "vertical resize " . (winwidth(0) * 2/3)<CR>
 " }}}
-]]
-)
+
+]])
