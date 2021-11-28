819a8cb3f9a7f01e750f0b13ffee91a2185fac57
PrashanthaTP <tpprashantha@gmail.com>
2021-11-28 16:23:04 +0530

    Feat(nvim): add support more lanugages with efm
    - also refactor some functions
    - formatters : autopep8,stylua,prettier
    tags:
    
        LSP, server, efm

--- .config/nvim/lua/plugins/nvim-lspconfig/servers/efm.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig/servers/efm.lua
@@ -1,4 +1,4 @@
-local lspconfig = require('lspconfig')
+local lspconfig = require("lspconfig")
 local M = {}
 
 --[===[
@@ -18,26 +18,57 @@ local black = { formatCommand = 'black --quiet -', formatStdin = true }
 --]===]
 --
 local lua_format_cmd = function()
-    local buf_name = vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))
-    return LUA_LANG_SERVER_EXE.." -i '"..buf_name.."'"
+	local buf_name = vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))
+	return FORMATTERS_DIR .. "/stylua -s '" .. buf_name .. "'"
 end
 
-local python_formatter = {formatCommand = "python -m autopep8 -", formatStdin = true}
+local prettier_format_cmd = function(lang)
+	local buf_name = vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))
+    local command = "prettier --stdin-filepath "..buf_name
+    if lang==nil or string.len(lang)==0 then
+        return command
+    end
+end
 
-local lua_formatter = {formatCommand = FORMATTERS_DIR.."/stylua -s -", formatStdin = true }
-local languages = { python={python_formatter}, lua={lua_formatter} }
-
-M.setup = function (on_attach)
-
-require "lspconfig".efm.setup {
-    init_options = {documentFormatting = true},
-    cmd ={LANGUAGE_SERVERS_DIR .."/efm-langserver"},
-    settings = {
-        languages = languages
-    },
-        single_file_support = true
+local python_formatter = { formatCommand = "python -m autopep8 -", formatStdin = true }
+local prettier_formatter = function(lang)
+    return { formatCommand = prettier_format_cmd(lang), formatStdin = true }
+end
+local lua_formatter = { formatCommand = FORMATTERS_DIR .. "/stylua -s -", formatStdin = true }
+local languages = {
+	css = { prettier_formatter("css") },
+	html = { prettier_formatter("html") },
+	javascript = { prettier_formatter("") },
+	javascriptreact = { prettier_formatter("") },
+	json = { prettier_formatter("json") },
+	lua = { lua_formatter },
+	python = { python_formatter },
+	typescript = { prettier_formatter("") },
+	typescriptreact = { prettier_formatter("") },
+	scss = { prettier_formatter("scss") },
 }
 
+M.setup = function(on_attach)
+	require("lspconfig").efm.setup({
+		filetypes = {
+			"css",
+			"html",
+			"javascript",
+			"javascriptreact",
+			"json",
+			"lua",
+			"python",
+			"typescript",
+			"typescriptreact",
+			"scss",
+		},
+		init_options = { documentFormatting = true },
+		cmd = { LANGUAGE_SERVERS_DIR .. "/efm-langserver" },
+		settings = {
+			languages = languages,
+		},
+		single_file_support = true,
+	})
 end
 
 return M
