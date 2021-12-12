42499382126b3903df84744394541ad56ffc31e0
PrashanthaTP <tpprashantha@gmail.com>
2021-12-12 23:13:23 +0530

    Feat(nvim): use lua-format for lua code-formatting
    also performed document formatting
    
    tags:
        code formatting, lua-format, LSP, servers

--- .config/nvim/lua/plugins/nvim-lspconfig/servers/efm.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig/servers/efm.lua
@@ -1,6 +1,5 @@
 local lspconfig = require("lspconfig")
 local M = {}
-
 --[===[
 --local eslint = {
   lintCommand = 'eslint_d -f unix --stdin --stdin-filename ${INPUT}',
@@ -10,7 +9,6 @@ local M = {}
   formatCommand = 'eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}',
   formatStdin = true,
 }
-
 local clang_format = { formatCommand = 'clang-format -style=LLVM ${INPUT}', formatStdin = true }
 local prettier = { formatCommand = 'prettier --stdin-filepath ${INPUT}', formatStdin = true }
 local stylua = { formatCommand = 'stylua -s -', formatStdin = true }
@@ -19,19 +17,18 @@ local black = { formatCommand = 'black --quiet -', formatStdin = true }
 --
 --
 local lua_format_cmd = function()
-  local buf_name = vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))
-  return FORMATTERS_DIR .. "/stylua -s '" .. buf_name .. "'"
+    local buf_name = vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))
+    return FORMATTERS_DIR .. "/stylua -s '" .. buf_name .. "'"
 end
-
 local prettier_format_cmd = function(lang)
-  local buf_name = vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))
-  local command = "prettier --stdin-filepath " .. buf_name
-  if lang == nil or string.len(lang) == 0 then
-    return command
-  end
+    local buf_name = vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))
+    local command = "prettier --stdin-filepath " .. buf_name
+    if lang == nil or string.len(lang) == 0 then return command end
 end
-
-local python_formatter = { formatCommand = "python -m autopep8 --indent-size=4 -", formatStdin = true }
+local python_formatter = {
+    formatCommand = "python -m autopep8 --indent-size=4 -",
+    formatStdin = true
+}
 --[===[
 --local prettier_formatter = function(lang)
 	return { formatCommand = prettier_format_cmd(lang), formatStdin = true }
@@ -39,74 +36,60 @@ end
 --]===]
 --
 local function eslint_config_exists()
-  local eslintrc = vim.fn.glob(".eslintrc*", 0, 1)
-
-  if not vim.tbl_isempty(eslintrc) then
-    return true
-  end
-
-  if vim.fn.filereadable("package.json") then
-    if vim.fn.json_decode(vim.fn.readfile("package.json"))["eslintConfig"] then
-      return true
+    local eslintrc = vim.fn.glob(".eslintrc*", 0, 1)
+    if not vim.tbl_isempty(eslintrc) then return true end
+    if vim.fn.filereadable("package.json") then
+        if vim.fn.json_decode(vim.fn.readfile("package.json"))["eslintConfig"] then
+            return true
+        end
     end
-  end
-
-  return false
+    return false
 end
-
 local eslint_formatter = {
-  lintCommand = "eslint_d -f unix --stdin --stdin-filename ${INPUT}",
-  lintStdin = true,
-  lintFormats = { "%f:%l:%c: %m" },
-  lintIgnoreExitCode = true,
-  formatCommand = "eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}",
-  formatStdin = true,
+    lintCommand = "eslint_d -f unix --stdin --stdin-filename ${INPUT}",
+    lintStdin = true,
+    lintFormats = {"%f:%l:%c: %m"},
+    lintIgnoreExitCode = true,
+    formatCommand = "eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}",
+    formatStdin = true
 }
 local prettier_formatter = {
-  formatCommand = "prettierd ${INPUT}",
-  formatStdin = true,
-  --rootPatterns = { ".git", "jsconfig.json", "tsconfig.json", "package.json" },
+    formatCommand = "prettierd ${INPUT}",
+    formatStdin = true
+    -- rootPatterns = { ".git", "jsconfig.json", "tsconfig.json", "package.json" },
 }
-local lua_formatter = { formatCommand = FORMATTERS_DIR .. "/stylua -s -", formatStdin = true }
-
+local lua_formatter = {
+    -- formatCommand = FORMATTERS_DIR .. "/stylua -s -",
+    formatCommand = FORMATTERS_DIR .. "/lua-format -i",
+    formatStdin = true
+}
 local languages = {
-  css = { prettier_formatter },
-  html = { prettier_formatter },
-  javascript = { prettier_formatter },
-  javascriptreact = { prettier_formatter },
-  json = { prettier_formatter },
-  lua = { lua_formatter },
-  python = { python_formatter },
-  typescript = { prettier_formatter },
-  typescriptreact = { prettier_formatter, eslint_formatter },
-  scss = { prettier_formatter },
+    css = {prettier_formatter},
+    html = {prettier_formatter},
+    javascript = {prettier_formatter},
+    javascriptreact = {prettier_formatter},
+    json = {prettier_formatter},
+    lua = {lua_formatter},
+    python = {python_formatter},
+    typescript = {prettier_formatter},
+    typescriptreact = {prettier_formatter, eslint_formatter},
+    scss = {prettier_formatter}
 }
-
 M.setup = function(on_attach)
-  require("lspconfig").efm.setup({
-    on_attach = function(client, bufnr)
-      client.resolved_capabilities.document_formatting = true
-      client.resolved_capabilities.goto_definition = false
-      on_attach(client, bufnr)
-    end,
-    filetypes = {
-      "css",
-      "html",
-      "javascript",
-      "javascriptreact",
-      "json",
-      "lua",
-      "python",
-      "typescript",
-      "typescriptreact",
-      "scss",
-    },
-    init_options = { documentFormatting = true },
-    cmd = { LANGUAGE_SERVERS_DIR .. "/efm-langserver" },
-    settings = {
-      languages = languages,
-    },
-    --[===[
+    require("lspconfig").efm.setup({
+        on_attach = function(client, bufnr)
+            client.resolved_capabilities.document_formatting = true
+            client.resolved_capabilities.goto_definition = false
+            on_attach(client, bufnr)
+        end,
+        filetypes = {
+            "css", "html", "javascript", "javascriptreact", "json", "lua",
+            "python", "typescript", "typescriptreact", "scss"
+        },
+        init_options = {documentFormatting = true},
+        cmd = {LANGUAGE_SERVERS_DIR .. "/efm-langserver"},
+        settings = {languages = languages},
+        --[===[
 		root_dir = function()
 			if not eslint_config_exists() then
 				return nil
@@ -114,10 +97,11 @@ M.setup = function(on_attach)
 			return vim.fn.getcwd()
 		end,
         --]===]
-    root_dir = lspconfig.util.root_pattern(".git", "package.json", ".stylua.toml", ".pep8") or vim.loop.cwd(),
-    --root_dir = vim.loop.cwd,
-    single_file_support = true,
-  })
+        root_dir = lspconfig.util.root_pattern(".git", "package.json",
+                                               ".stylua.toml", ".pep8") or
+            vim.loop.cwd(),
+        -- root_dir = vim.loop.cwd,
+        single_file_support = true
+    })
 end
-
 return M
